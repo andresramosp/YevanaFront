8e8112540b50705510104730a905e664f47d31f7
Andrés Ramos <andreschennai@gmail.com>
2020-02-23 13:52:56 +0100

    nuxtjs device for generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,10 @@ export default {
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-    '@nuxtjs/device',
+    [
+      '@nuxtjs/device',
+      {defaultUserAgent: 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36'}
+    ]
   ],
   /*
   ** Axios module configuration
