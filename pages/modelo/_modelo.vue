3374052fa71a35fe64439489f8f06f804f53360b
andresrp <andreschennai@gmail.com>
2020-06-23 19:41:41 +0200

    nueva seccion

--- pages/modelo/_modelo.vue
+++ pages/modelo/_modelo.vue
@@ -4,39 +4,30 @@
       <div>
         <video id="myVideo" controls width="100%" allow="autoplay" preload="auto" muted>
           <source
-            src="https://www.dropbox.com/s/ff14kruh25mwuxz/videoDockerMobile.mp4?raw=1"
+            src="https://www.dropbox.com/s/7rqsuv14jc5ylf4/videoDockerMobile2.mp4?raw=1"
             type="video/mp4"
           />
         </video>
       </div>
     </section>
     <div class="previews-wrap">
-      <div>
-        <div class="preview-item false">
-          
+      <div v-for="(mosaic, index) in mosaicItems" :key="index">
+        <div :class="`preview-item ${index % 2 ? 'right-img' : 'false'}`">
           <div class="preview-item__content">
-            <h3>Vehicles that fit your lifestyle</h3>
-            <p>Find the best car for your life in just a few clicks, &amp; see true vehicle details and standard features listed.</p>
+            <h3>{{mosaic.title}}</h3>
+            <p style="margin-top: 5px;">
+              <span v-html="mosaic.text"></span>
+            </p>
+            <div class="icon-list">
+               <img
+                v-for="(icon, index) in mosaic.icons" :key="index"
+                :src="require('~/assets/icons/' + icon.toLowerCase() +'.png')"
+                :width="35"
+                :height="35"
+              />
+            </div>
           </div>
-          <img alt="car" src="https://caroogo.com/static/img/home_page_car_buying_img1.jpg" />
-        </div>
-      </div>
-      <div>
-        <div class="preview-item right-img">
-          <div class="preview-item__content">
-            <h3>Pricing &amp; Financing without Haggling</h3>
-            <p>Calculate, customize, and compare bottom line pricing right off the bat &amp; get no risk, pre-qualification in minutes.</p>
-          </div>
-           <img alt="car" src="https://caroogo.com/static/img/home_page_car_buying_img2.jpg" />
-        </div>
-      </div>
-      <div>
-        <div class="preview-item false">
-          <div class="preview-item__content">
-            <h3>Find &amp; Buy on Your Terms and Time</h3>
-            <p>Since you’re the one behind the wheel, you can take charge of your car buying experience from selecting a vehicle &amp; customizing payment, all the way through scheduling delivery.</p>
-          </div>
-           <img alt="car" src="https://caroogo.com/static/img/home_page_car_buying_img3.jpg" />
+          <img alt="car" :src="`/img/modelos/docker/mosaico_${index + 1}.jpg`" />
         </div>
       </div>
     </div>
@@ -52,7 +43,52 @@ export default {
   mixins: [homeMixins],
   async asyncData({ params }) {
     return {
-      modelo: params.modelo
+      modelo: params.modelo,
+      mosaicItems: [
+        {
+          title: "ADAPTARSE ES GANAR TERRENO",
+          icons: ['gps', 'cama', 'cartucho_de_gas', 'toalla_grande'],
+          text:
+            "Un viaje tiene muchos aspectos y sabemos que el auténtico viajero quiere disfrutarlos todos al máximo. Por eso, presentamos nuestro nuevo modelo All-Road: un vehículo que se adapta a todo tipo de viajes"
+        },
+        {
+          title: "EL DESCANSO DE TUS SUEÑOS",
+          icons: ['asiento', 'cama', 'silla_de_camping'],
+          text:
+            "Cama doble de 1.95x1.1m, colchoneta estilo japonés ultraﬁna (65mm) confeccionada con 3 capas de diferente densidad, plegable con bisagras de tejido. Tapicería a escoger o deﬁnida según personalización."
+        },
+        {
+          title: "ESPACIO PARA TODO TU MUNDO",
+          icons: ['cocina', 'gps'],
+          icons: ['electricidad', 'radio'],
+          text:
+            "Armarios, altillos, compartimentos y baúl con capacidad para 50 litros. Brazos neumáticos de asistencia para la apertura de puertas.<br /> Cierres de seguridad Push Lock metálicos."
+        },
+        {
+          title: "UNA ILUMINACIÓN PARA DESCONECTAR",
+          icons: ['toldo', 'portabicis', 'segundo_conductor'],
+          text:
+            "Iluminación ambiental sectorizada regulable en 3 zonas: Exterior lateral cocina, Exterior zona maletero, interior general (focosDownligth y tira led) Iluminación automática de todos los compartimentos con puertas. <br /> <br /> Foco led de lectura"
+        },
+        {
+          title: "PENSADA PARA VIAJAR, EQUIPADA PARA TODO",
+          icons: ['cocina', 'gps'],
+          text:
+            "Nevera / Congelador Mobicool FR34 a compresor, baja hasta -10ºC. <br />Guías de cajones y tornillería Wurth, tornillería INOX Cocina a Gas de 1 fuego Butsir MS 100 PRO. <br />Compartimento especíﬁco para el gas de la cocina Fregadero INOX, tamaño…  grifo RK Reich STYLE"
+        },
+        {
+          title: "UN LUGAR DONDE RECARGAR ENERGÍAS",
+          icons: ['pack_dormir', 'cocina'],
+          text:
+            "Placa solar CARBEST 100W <br />Batería tecnología AGM 120Ah <br />Centralita de control del sistema NASA MARINE <br />Sistema de carga con Booster Votronic de 30A para carga rápida de batería en ruta. <br />Claraboya de climatización Turbovent Premium <br />Cristal 390x390mm con mosquitera integrada y oscurecedor"
+        },
+        {
+          title: "LITERALMENTE, NO HABRÁ OTRA IGUAL",
+          icons: ['cocina', 'gps'],
+          text:
+            "Ofrecemos una amplia variedad de extras para que elijas cada detalle de tu camper. <br />Para que disfrutes de todos tus viajes sabiendo que los realizas en un vehículo realmente único. "
+        }
+      ]
     };
   },
   mounted() {
@@ -86,86 +122,95 @@ export default {
   margin-top: -6px;
 }
 .preview-item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .preview-item > img {
-    width: 50%;
+  width: 50%;
 }
 img {
-    vertical-align: middle;
-    border-style: none;
+  vertical-align: middle;
+  border-style: none;
 }
 .preview-item__content {
-    position: relative;
-    width: 50%;
-    padding: 0 2.1875rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  position: relative;
+  width: 50%;
+  padding: 0 2.1875rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 .preview-item__content p {
-    font-style: normal;
-    font-weight: normal;
-    font-size: 1rem;
-    line-height: 1.5rem;
-    width: 28.75rem;
-    max-width: 100%;
-    padding-right: 0.9375rem;
-    color: #A1AEB7;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 22px;
+  width: 80%;
+  max-width: 100%;
+  padding-right: 0.9375rem;
+  color: black;
 }
 .preview-item__content h3 {
-    width: 28.75rem;
-    max-width: 100%;
-    color: #505D68;
-    font-weight: bold;
-    font-size: 1.5rem;
-    line-height: 1.938rem;
-    text-transform: capitalize;
-    margin-bottom: 0.9375rem;
+  width: 80%;
+  max-width: 100%;
+  color: #505d68;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 1.938rem;
+  margin-bottom: 0.9375rem;
 }
 
 .preview-item.right-img {
-    flex-direction: row-reverse;
+  flex-direction: row-reverse;
 }
+
 @media (max-width: 991px) {
   .preview-item {
-      display: block;
+    display: block;
   }
-}
-@media (max-width: 767px) {
-  .preview-item {
-      flex-flow: column nowrap;
-  }
-}
-@media (max-width: 991px)
-{
   .preview-item__content {
-      width: 100%;
-      padding: 1.875rem 1.875rem 3.125rem;
-      align-items: flex-start;
-      justify-content: flex-start;
+    width: 100%;
+    height: 375px;
+    padding-top: 55px;
+    padding-bottom: 55px;
+    /* padding-right: 45px;
+      padding-left: 45px; */
+    align-items: flex-start;
+    justify-content: flex-start;
   }
-}
-@media (max-width: 991px) {
+
+  .preview-item__content h3 {
+    font-size: 20px;
+    width: 78%;
+  }
+
   .preview-item__content p {
-      width: 100%;
+    font-size: 13px;
+    width: 100%;
+    line-height: 20px;
+  }
+
+  .preview-item > img {
+    width: 100%;
   }
 }
 
 @media (max-width: 767px) {
   .preview-item.right-img {
-      flex-direction: column;
+    flex-direction: column;
+  }
+  .preview-item {
+    flex-flow: column nowrap;
   }
 }
-
-@media (max-width: 991px) {
-.preview-item > img {
-    width: 100%;
+.icon-list {
+  margin-top: 30px;
+  padding-left: 15px;
+  padding-right: 15px;
 }
+.icon-list img {
+  margin-right: 20px
 }
-
-
 </style>
